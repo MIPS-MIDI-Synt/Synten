74e3c6504589761239e1f3414f3ec2d8a2a8a53d
Martin Lindefors <lindefor@kth.se>
2021-11-25 15:42:54 +0100

    Enabled and configured Uart interrupts, needs some additional work

--- UART/main.c
+++ UART/main.c
@@ -35,17 +35,25 @@ int main(void) {
 	unsigned char tmp;
 	delay(10000000);
 	ODCE = 0; // Bara output?
-	TRISECLR = 0xFF;
+	TRISECLR = 0xFF; // ??
 
 	init();
 	
 	/* Configure UART1 for 115200 baud, no interrupts */
 	U1BRG = calculate_baudrate_divider(80000000, 31250, 0); // Initializes U1BRG register for 31250 baud
-	U1STA = 0; // ???
+	U1STA = 0; // Varför nollas denna?
 	/* 8-bit data, no parity, 1 stop bit */
-	U1MODE = 0x8000; 
+	U1MODE = 0x8000; // Enable UART module by setting the ON bit
 	/* Enable transmit and recieve */
 	U1STASET = 0x1400;	// Only enable receive
+
+	// Interrupts:
+	IEC(0) |= 0x08000000; // Interrupt enable bit 27 in IEC0 
+	IPC(6) |= 0x1F; // Interupt priority max = 7 IPC6<4:2>, sup-priority max = 3 IPC6<1:0>
+	// OBS: Below conflicting with row 44,46
+	U1STA |= 0xC0; // Receive interrupt mode = 11 > Flag when buffer becomes full (One word = One MIDI message)
+	// Interrupt flag @ IFS0<27>
+	// xxxx xxxx xxxx xxxx xxxx xxxx 11xx xxxx
 
 	for (;;) { // Uses polling, might switch to interrupts, also ignore write buffer and U1TXREG
 		while(!(U1STA & 0x1)); //wait for read buffer to have a value
