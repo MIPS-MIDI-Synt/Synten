a139ef199fdba717122d84362f141955680f0fbc
Marcus <warner@kth.se>
2021-11-27 13:13:14 +0100

    Updated midi messages

--- UART/MIDI/midihandler.c
+++ UART/MIDI/midihandler.c
@@ -1,4 +1,18 @@
 #include <pic32mx.h>
+#include <math.h>
+
+#define BUFFER_SIZE 1024
+#define SAMPLE_RATE 44100
+#define SAMPLE_DURATION (1.0f / SAMPLE_RATE)
+#define STREAM_BUFFER_SIZE 1024
+#define NUM_OSCILLATORS 32
+
+void message_handler(int midimessage){
+	int midi_func = (midimessage & 0x00f00000) >> 24; 
+	int midi_chan = (midimessage & 0x000f0000) >> 16;
+	int midi_key  = (midimessage & 0x00007f00) >> 8;
+	int midi_vel  = (midimessage & 0x0000007f); 
+}
 
 void delay(int cyc) {
 	int i;
@@ -35,15 +49,15 @@ int main(void) {
 	unsigned char tmp;
 	delay(10000000);
 	ODCE = 0; // Bara output?
-	TRISECLR = 0xFF; // ??
+	TRISECLR = 0xFF;
 
 	init();
 	
 	/* Configure UART1 for 115200 baud, no interrupts */
 	U1BRG = calculate_baudrate_divider(80000000, 31250, 0); // Initializes U1BRG register for 31250 baud
-	U1STA = 0; // Varför nollas denna?
+	U1STA = 0; // ???
 	/* 8-bit data, no parity, 1 stop bit */
-	U1MODE = 0x8000; // Enable UART module by setting the ON bit
+	U1MODE = 0x8000; 
 	/* Enable transmit and recieve */
 	U1STASET = 0x1400;	// Only enable receive
 
@@ -51,9 +65,9 @@ int main(void) {
 	IEC(0) |= 0x08000000; // Interrupt enable bit 27 in IEC0 
 	IPC(6) |= 0x1F; // Interupt priority max = 7 IPC6<4:2>, sup-priority max = 3 IPC6<1:0>
 	// OBS: Below conflicting with row 44,46
-	U1STA |= 0xC0; // Receive interrupt mode = 11 > Flag when buffer becomes full (One word = One MIDI message)
+	U1STA |= 0x80; // Receive interrupt mode = 10 > Flag when buffer becomes 3/4 (24bits = One MIDI message)
 	// Interrupt flag @ IFS0<27>
-	// xxxx xxxx xxxx xxxx xxxx xxxx 11xx xxxx
+	// xxxx xxxx xxxx xxxx xxxx xxxx 10xx xxx
 
 	for (;;) { // Uses polling, might switch to interrupts, also ignore write buffer and U1TXREG
 		while(!(U1STA & 0x1)); //wait for read buffer to have a value
